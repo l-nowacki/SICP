abdc953a6617cd1efccfdaeb9c8e08d62bf41004
nowacki <luasantilli@gmail.com>
2019-03-24 12:49:23 -0300

    Finalized the exercise set5. The problem 1.15 was yet to be solved

--- exercises/set5_1_14_to_1_15.scm
+++ exercises/set5_1_14_to_1_15.scm
@@ -32,6 +32,39 @@
   (- (* 3 x) (* 4 (cube x))))
 
 (define (sine angle)
-  (if (not (> (abs angle) 0.1))
+  (if (<= (abs angle) 0.1)
       angle
       (p (sine (/ angle 3.0)))))
+
+;; a
+
+(define (<= a b)
+  (not (> a b)))
+
+(define (>= a b)
+  (not (< a b)))
+
+;; The amount of times the p will applied is equal to
+;; amount of times you need to divide the angle by 3
+;; to get the angle to be smaller or equal to 0.1.
+;; Therefore the procedure helper should suffice in order
+;; to discover the amount of times p is applied.
+(define (helper n)
+  (define (iter n count)
+    (if (<= n 0.1)
+	count
+	(iter (/ n 3) (+ count 1))))
+  (iter n 0))
+
+;; (helper 12.15) gives 5 as a result. Hence p is applied 5 times.
+
+;; b
+
+;; The order of growth in space and number of steps (as a function of a) is
+;; proportional to log_3(* 10 a)
+;; This happens because the amount of steps necessary to return the final
+;; value of the function is proportional to the amount of times needed to
+;; divide a by 3 to get a value smaller or equal to 0.1
+
+
+
